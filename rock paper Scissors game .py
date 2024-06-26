f4ba095838a70940c56bcaa322386223c0736da1
AdityaBramhwanshi <156591898+AdityaBramhwanshi@users.noreply.github.com>
2024-06-26 15:21:57 +0530

    Add files via upload
    Rock , Paper or Scissors is a game where two players are involved and this game is played under certain rules that rock beats scissors , scissor cuts the paper and paper covers the rock and the players make their choices and show whether they win or loose  .

---
+++ rock paper Scissors game .py
@@ -0,0 +1,29 @@
+import random
+options = ["Rock", "Paper", "Scissors"]
+while True:
+    
+    user_choice = input("Choose Rock, Paper, or Scissors: ")
+    computer_choice = random.choice(options)
+    print("You chose: ", user_choice)
+    print("Computer chose: ", computer_choice)
+    if user_choice == computer_choice:
+        print("It's a tie!")
+
+    elif user_choice == "Rock" and computer_choice == "Scissors":
+        print("You win!")
+
+    elif user_choice == "Paper" and computer_choice == "Rock":
+        print("You win!")
+
+    elif user_choice == "Scissors" and computer_choice == "Paper":
+        print("You win!")
+
+    else:
+        print("Computer wins!")
+    next = input("Let's Play Again ? (yes/no): ")
+    if next == "no":
+          break
+else:
+        print("Invalid Input")    
+   
+    
